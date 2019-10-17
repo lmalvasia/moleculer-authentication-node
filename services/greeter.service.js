0e7b484dc6c15259ef5a9ed7fd0ce667cbf7c472
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-10-17 15:08:21 -0300

    Authentication: Initial structure

---
+++ services/greeter.service.js
@@ -0,0 +1,78 @@
+"use strict";
+
+module.exports = {
+	name: "Test.greeter",
+
+	/**
+	 * Service settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Service dependencies
+	 */
+	dependencies: [],	
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello'
+		 *
+		 * @returns
+		 */
+		hello() {
+			return "Hello Moleculer";
+		},
+
+		/**
+		 * Welcome a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			handler(ctx) {
+				return `Welcome ${ctx.params.payload.name}`;
+			}
+		}
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
